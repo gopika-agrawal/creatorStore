4b583185837eeb5a31de135934b600b3656cc77c
Gopika Agrawal <gopikagoyal3336@gmail.com>
2026-05-23 23:18:55 +0530

    initial setup

---
+++ src/main/java/com/example/creatorstore/CreatorstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.creatorstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatorstoreApplication.class, args);
+	}
+
+}
